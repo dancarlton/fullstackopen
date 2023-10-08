69bec1fbd21c93e2ef7423f725e7fe866078fb2c
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-08 15:53:55 -0700

    part1 unicafe

---
+++ part1/greetings/src/App.jsx
@@ -0,0 +1,25 @@
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const name = 'Dan';
+  const age = 29;
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+};
+
+export default App;
