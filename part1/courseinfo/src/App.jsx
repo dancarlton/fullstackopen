ed8f430e360f41943a0c60afd49a521013a71242
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-11 17:05:24 -0700

    courseinfo ex 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,50 +1,45 @@
 const Header = (course) => {
-
   return (
     <div>
       <h1>{course.course}</h1>
     </div>
-  )
-}
+  );
+};
 
 const Content = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  return (
+    <div>
+      <Part part='Fundamentals of React' exercises={10} />
+      <Part part='Using props to pass data' exercises={7} />
+      <Part part='State of a component' exercises={14} />
+    </div>
+  );
+};
 
+const Part = (props) => {
   return (
     <div>
       <p>
-        {part1} {exercises1}
+        {props.part} {props.exercises}
       </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-
     </div>
-  )
-}
+  );
+};
 
 const Total = () => {
-  const exercises1 = 10
-  const exercises2 = 7
-  const exercises3 = 14
+  const exercises1 = 10;
+  const exercises2 = 7;
+  const exercises3 = 14;
 
   return (
-  <div>
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  </div>
-  )
-}
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
+  const course = 'Half Stack application development';
 
   return (
     <div>
@@ -52,7 +47,7 @@ const App = () => {
       <Content />
       <Total />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
