8ca12fce34389e59efd41cc98c13e7f997029d22
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-12 19:04:54 -0700

    counter

--- part1/counter/src/App.jsx
+++ part1/counter/src/App.jsx
@@ -2,8 +2,9 @@ import { useState } from 'react';
 
 const Display = ({ counter }) => <div>{counter}</div>;
 
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
-
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
   const [counter, setCounter] = useState(0);
