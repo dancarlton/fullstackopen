8d8eacfa09a06bc46523a13ed915e543f489463a
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-12 19:04:33 -0700

    complex state

--- part1/complexstate/src/App.jsx
+++ part1/complexstate/src/App.jsx
@@ -1,21 +1,46 @@
 import { useState } from 'react';
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(' ')}</div>;
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    console.log('left after', left);
+    setRight(right + 1);
+    setTotal(left + right);
+  };
 
   return (
     <div>
       {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
       {right}
+      <p>{allClicks.join(' ')}</p>
+      <History allClicks={allClicks} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
