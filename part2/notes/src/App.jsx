52d7f7e65d5d18ca25e9e97334d5c79889607271
Dan Carlton <danlouis.carlton@gmail.com>
2023-10-15 22:10:30 -0700

    updated notes

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -4,10 +4,18 @@ import Note from '../components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
   const [newNote, setNewNote] = useState('a new note...')
+  const [showAll, setShowAll] = useState(true)
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
   const handleNoteChange = (event) => {
@@ -15,11 +23,18 @@ const App = (props) => {
     setNewNote(event.target.value)
   }
 
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -7,18 +7,18 @@ const notes = [
   {
     id: 1,
     content: 'HTML is easy',
-    important: true
+    important: true,
   },
   {
     id: 2,
     content: 'Browser can execute only JavaScript',
-    important: false
+    important: false,
   },
   {
     id: 3,
     content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
+    important: true,
+  },
 ]
 
 ReactDOM.createRoot(document.getElementById('root')).render(
