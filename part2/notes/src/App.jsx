ada02634d41cc776cb43b5d7fe47ad4f088ff09d
Dan Carlton <danlouis.carlton@gmail.com>
2023-11-08 14:10:12 -0800

    updated notes

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from '../components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/notes').then(response => {
+      console.log('promise fulfilled')
+      setNotes(response.data)
+    })
+  }, [])
+
+  const addNote = event => {
     event.preventDefault()
     const noteObject = {
       content: newNote,
@@ -18,12 +27,12 @@ const App = (props) => {
     setNewNote('')
   }
 
-  const handleNoteChange = (event) => {
+  const handleNoteChange = event => {
     setNewNote(event.target.value)
     console.log(event.target.value)
   }
 
-  const notesToShow = showAll ? notes : notes.filter((note) => note.important)
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
   return (
     <div>
@@ -34,7 +43,7 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => (
+        {notesToShow.map(note => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
--- part2/notes/src/main.jsx
+++ part2/notes/src/main.jsx
@@ -1,15 +1,5 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-
 import App from './App'
-import axios from 'axios'
-
-const promise = axios
-  .get('http://localhost:3001/notes')
-  .then(response => {
-    const notes = response.data
-    console.log(notes)
-})
 
 const notes = [
   {
@@ -29,6 +19,4 @@ const notes = [
   },
 ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
