3a630ad771bfdab0615613b4d2e63ba7b2ab8c37
Dan Carlton <danlouis.carlton@gmail.com>
2024-05-09 16:21:40 -0700

    part3 backend

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 
 let notes = [
   {
@@ -20,14 +22,69 @@ let notes = [
   }
 ]
 
+// find url
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello Dan!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+// generate new id and create new notes
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
 })
 
+// find a note
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if(note){
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+// delete a note
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.filter(note => note.id != id)
+
+response.status(204).end()
+})
+
+// add a note
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
+})
+
+
+
+// configure the url PORT
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
