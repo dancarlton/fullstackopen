784ae3b252a11c85a923ed0bc9b64785a2296e20
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-22 18:15:11 -0700

    updated baseUrl

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://fullstackopen-part3-d1g8.onrender.com/api/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
--- part3/backend/index.js
+++ part3/backend/index.js
@@ -2,9 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors({
-  origin: 'http://localhost:3000'
-}))
+app.use(cors())
 app.use(express.json())
 
 let notes = [
