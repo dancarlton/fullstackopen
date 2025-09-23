5a2cadd93f071e239dcc46c585d9738ee428bca1
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-22 19:04:57 -0700

    updated phonebook to create Render server

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,31 +1,31 @@
-import axios from "axios";
-const baseUrl = 'http://localhost:3001/persons'
+import axios from 'axios'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
-    return axios.get(baseUrl)
-      .then(response => response.data)
-      .catch(err => {
-        console.log("API Error:", err);
-        throw err;
-      });
-  }
-
+  return axios
+    .get(baseUrl)
+    .then(response => response.data)
+    .catch(err => {
+      console.log('API Error:', err)
+      throw err
+    })
+}
 
 const create = newObject => {
-    return axios.post(baseUrl, newObject)
+  return axios.post(baseUrl, newObject)
 }
 
 const update = (id, newObject) => {
-    return axios.put(`${baseUrl}/${id}`, newObject)
+  return axios.put(`${baseUrl}/${id}`, newObject)
 }
 
-const deletePerson = (id) => {
-    return axios.delete(`${baseUrl}/${id}`)
+const deletePerson = id => {
+  return axios.delete(`${baseUrl}/${id}`)
 }
 
 export default {
-    getAll,
-    create,
-    update,
-    deletePerson
+  getAll,
+  create,
+  update,
+  deletePerson,
 }
