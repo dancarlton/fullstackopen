d9622a312edc85884e8ed05323a8fc477c65746d
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-24 15:29:40 -0700

    uibuild

--- part3/phonebook/models/phonebook.js
+++ part3/phonebook/models/phonebook.js
@@ -14,8 +14,22 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`,
+    },
+  },
 })
 
 personSchema.set('toJSON', {
@@ -23,7 +37,7 @@ personSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  }
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
