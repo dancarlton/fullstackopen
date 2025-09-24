935085d60840a62c9bc8cccb9f93d09a1a9eb291
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-24 14:18:57 -0700

    completed 3.13 - 31.4

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,5 +1,32 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const url = `mongodb+srv://danc:${password}@phonebook.g3dwcq5.mongodb.net/notesApp?retryWrites=true&w=majority&appName=phonebook`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 app.use(express.json())
 app.use(express.static('dist'))
@@ -7,19 +34,19 @@ app.use(express.static('dist'))
 let notes = [
   {
     id: 1,
-    content: "HTML is easy",
-    important: true
+    content: 'HTML is easy',
+    important: true,
   },
   {
     id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
+    content: 'Browser can execute only JavaScript',
+    important: false,
   },
   {
     id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
 ]
 
 // find url
@@ -29,14 +56,14 @@ app.get('/', (request, response) => {
 
 // get notes
 app.get(`/api/notes`, (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 // generate new id and create new notes
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id))
-    : 0
+  const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
   return maxId + 1
 }
 
@@ -45,20 +72,17 @@ app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
+    return response.status(400).json({ error: 'content missing' })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: Boolean(body.important) || false,
-    id: generateId(),
-  }
+    important: body.important || false,
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 // find a note
@@ -66,29 +90,33 @@ app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
 
-  if(note){
+  if (note) {
     response.json(note)
   } else {
     response.status(404).end()
   }
 })
 
-// delete a note
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+// get a note by id
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-
-// app.post('/api/notes', (request, response) => {
-//   const note = request.body
-//   console.log(note)
-//   response.json(note)
-// })
-
-
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 // configure the url PORT
 const PORT = process.env.PORT || 3001
--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/phonebook')
 
 const app = express()
 
@@ -17,32 +19,62 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
-let persons = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-]
-
-// get all persons
+// display all persons
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+
+// add a person
+app.post('/api/persons', (req, res) => {
+  const {name, number } = req.body
+  const randomId = Math.floor(Math.random() * 1000000)
+
+  if (!name || !number) {
+    return res.status(400).json({ error: 'content missing' })
+  }
+
+  // check uniqueness
+  Person.findOne({ name })
+  .then(existingPerson => {
+      if (existingPerson) {
+        return res.status(400).json({ error: 'name bust be unique' }).end()
+      } else {
+
+        const person = new Person({
+          id: randomId,
+          name,
+          number,
+        })
+
+        person.save().then(savedPerson => res.json(savedPerson))
+      }
+    })
+
+})
+
+// get individual person
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  Person.findById(id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  })
+})
+
+// delete a person
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id
+  Person.findByIdAndDelete(id)
+  .then(person => {
+    if (person) res.status(204).end()
+  })
+
+
 })
 
 // get info
@@ -54,53 +86,9 @@ app.get('/info', (req, res) => {
         <p>${date}</p>`)
 })
 
-// get individual person
-app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
-    res.json(person)
-  } else {
-    res.status(404).end()
-  }
-})
-
-// delete a person
-app.delete('/api/persons/:id', (req, res) => {
-  const id = req.params.id
-  persons = persons.filter(person => person.id !== id)
-
-  res.status(204).end()
-})
-
-// add a person
-app.post('/api/persons', (req, res) => {
-  const body = req.body
-  const randomId = Math.floor(Math.random() * 1000000)
-
-  if (!body.name || !body.number) {
-    return res.status(400).json({ error: 'content missing' })
-  }
-
-  if (persons.some(person => person.name === body.name)) {
-    return res.status(400).json({ error: 'name bust be unique' })
-  }
-
-  const person = {
-    id: randomId,
-    name: body.name,
-    number: body.number,
-  }
-
-  persons = [...persons, person]
-
-  res.json(person)
-})
-
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
