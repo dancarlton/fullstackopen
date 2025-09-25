ab3136d01af9ebab612f97daae19462615f288d2
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-24 20:45:01 -0700

    added prop-types validation to remove error

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types'
+
 export default function Notification({ message, type }) {
   if (message === null) {
     return null
   }
 
   return <div className={type}>{message}</div>
+}
+
+
+Notification.propTypes = {
+  message: PropTypes.string,
+  type: PropTypes.string
 }
