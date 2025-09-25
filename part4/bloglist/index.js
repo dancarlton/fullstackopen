974b31828a5d81acf40385b7fb024957945be197
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-24 21:01:41 -0700

    completed 4.1

---
+++ part4/bloglist/index.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = process.env.MONGO_URI
+mongoose.connect(mongoUrl)
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then(result => {
+    response.status(201).json(result)
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
