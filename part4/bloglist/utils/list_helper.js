6fd21b98314b6f51b5b516623810ce242b5598fb
Dan Carlton <danlouis.carlton@gmail.com>
2025-09-24 22:25:54 -0700

    completed 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,20 @@ const totalLikes = blogs => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return null
+
+  let favorite = blogs[0]
+  blogs.forEach(blog => {
+    if (blog.likes > favorite.likes){
+        favorite = blog
+    }
+  });
+
+  return favorite.title
+}
+
 module.exports = {
   totalLikes,
+  favoriteBlog,
 }
